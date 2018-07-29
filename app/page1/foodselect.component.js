246e1358c9d78ea4b03e55099e352d9360498ac6
Andrew <ataunton@hotmail.com>
2018-07-29 20:33:31 +1000

    package update

--- app/page1/foodselect.component.js
+++ app/page1/foodselect.component.js
@@ -3,8 +3,8 @@ angular.
     component('foodselect',{
         templateUrl: 'page1/foodselectsite.html',
         controller: ['$http', function FoodListController($http) {
+            
             var self = this;
-
             $http.get('/foods/foods.json').then(function(response) {
                 self.food = response.data;
             });
