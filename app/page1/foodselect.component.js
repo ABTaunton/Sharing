26011f001bbf0381b2579ef4689bb66c1df40d30
Andrew <ataunton@hotmail.com>
2018-07-31 23:33:11 +1000

    Connect to external service

--- app/page1/foodselect.component.js
+++ app/page1/foodselect.component.js
@@ -5,7 +5,7 @@ angular.
         controller: ['$http', function FoodListController($http) {
             
             var self = this;
-            $http.get('//localhost:9000/food').then(function(response) {
+            $http.get('http://ec2-13-238-161-127.ap-southeast-2.compute.amazonaws.com:9000/food').then(function(response) {
                 self.foodName = response.data;
             });
           }
